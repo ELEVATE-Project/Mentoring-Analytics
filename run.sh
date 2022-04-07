68b5204d2e31039019e28bff6baa6089f3acff71
Shakthieshwari <shakthieshwari@shikshalokam.org>
2022-04-07 18:16:09 +0530

    pip path change in run.sh

--- run.sh
+++ run.sh
@@ -9,7 +9,7 @@ echo ""
 echo "$(date)"
 echo "====================================="
 echo "MentorEd - Daily User and Session Reports Execution == Started"
-. /opt/analytics/spark_venv/bin/activate && /opt/analytics/spark_venv/lib/python3.7/site-packages/pyspark/bin/spark-submit --packages org.apache.hadoop:hadoop-aws:2.7.7 /opt/analytics/Mentoring-Analytics/pyspark_user_session_batch.py
+. /opt/analytics/spark_venv/bin/activate && /opt/analytics/spark_venv/lib/python3.7/site-packages/pyspark/bin/spark-submit --packages org.apache.hadoop:hadoop-aws:2.7.7 /opt/analytics/Mentoring-Analytics/reports/pyspark_user_session_batch.py
 echo "MentorEd - Daily User and Session Reports Execution == Completed"
 echo "*************************************"
 
